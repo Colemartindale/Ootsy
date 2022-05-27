376eeea70a68a5616a88c348fa62c1236c3d27ef
Cole Martindale <cole.martindale13@gmail.com>
2022-05-27 09:27:23 -0700

    Add favicon

--- frontend/components/footer_container.js
+++ frontend/components/footer_container.js
@@ -2,9 +2,6 @@ import { connect } from "react-redux";
 import { openModal } from "../actions/modal_actions";
 import Footer from "./footer";
 
-// const mSTP = state => ({
-    
-// });
 
 const mDTP = dispatch => ({
     openModal: type => dispatch(openModal(type))
