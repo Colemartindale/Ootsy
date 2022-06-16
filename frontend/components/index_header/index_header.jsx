b7db300e1b19831fc013455c1a2a90cb4ac344b9
Cole Martindale <cole.martindale13@gmail.com>
2022-06-16 11:13:39 -0700

    Add media query for index header

--- frontend/components/index_header/index_header.jsx
+++ frontend/components/index_header/index_header.jsx
@@ -29,28 +29,52 @@ class ListingIndexHeader extends React.Component {
                     <span className="header-welcome">{welcomeMsg}</span>
                 <div className="index-header">
                     <ul className="prof-ul">
-                        <li>
-                            <img src={listings[3].photoUrl} alt='jewelry' onClick={() => this.props.history.push(`/listings/category/accessories`)}/>
+                        <li className="jewelry-li">
+                            <img 
+                                src={listings[3].photoUrl} 
+                                alt='jewelry' 
+                                onClick={() => this.props.history.push(`/listings/category/accessories`)}
+                            />
                             <span>Jewelry</span>
                         </li>
-                        <li>
-                            <img src={listings[10].photoUrl} alt='clothing' onClick={() => this.props.history.push(`/listings/category/clothing`)}/>
+                        <li className="clothing-li">
+                            <img 
+                                src={listings[10].photoUrl} 
+                                alt='clothing' 
+                                onClick={() => this.props.history.push(`/listings/category/clothing`)}
+                            />
                             <span>Clothing</span>
                         </li>
-                        <li>
-                            <img src={listings[15].photoUrl} alt='home' onClick={() => this.props.history.push(`/listings/category/home`)}/>
+                        <li className="home-li">
+                            <img 
+                                src={listings[15].photoUrl} 
+                                alt='home' 
+                                onClick={() => this.props.history.push(`/listings/category/home`)}
+                            />
                             <span>Decor</span>
                         </li>
-                        <li>
-                            <img src={listings[27].photoUrl} alt='toys' onClick={() => this.props.history.push(`/listings/category/toys`)}/>
+                        <li className="toys-li">
+                            <img 
+                                src={listings[27].photoUrl} 
+                                alt='toys' 
+                                onClick={() => this.props.history.push(`/listings/category/toys`)}
+                            />
                             <span>Toys</span>
                         </li>
-                        <li>
-                            <img src={listings[39].photoUrl} alt='smash' onClick={() => this.props.history.push(`/listings/category/smash`)}/>
+                        <li className="smash-li">
+                            <img 
+                                src={listings[39].photoUrl} 
+                                alt='smash' 
+                                onClick={() => this.props.history.push(`/listings/category/smash`)}
+                            />
                             <span>Smash</span>
                         </li>
-                        <li>
-                            <img src={listings[42].photoUrl} alt='electronics' onClick={() => this.props.history.push(`/listings/category/electronics`)}/>
+                        <li className="electronics-li">
+                            <img 
+                                src={listings[42].photoUrl} 
+                                alt='electronics' 
+                                onClick={() => this.props.history.push(`/listings/category/electronics`)}
+                            />
                             <span>Electronics</span>
                         </li>             
                     </ul>
